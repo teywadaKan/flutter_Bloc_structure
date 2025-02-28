7b957cbd8c08d351c96fb80c87d121efb356711f
KanDemonEyes <126938915+teywadaKan@users.noreply.github.com>
2025-02-28 13:40:52 +0700

    commit 2

--- example/lib/page/home_page/home_page.dart
+++ example/lib/page/home_page/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:example/bloc/counter/counter_bloc.dart';
 import 'package:example/cubit/counter_cubit.dart';
 import 'package:example/widgets/inc_and_dec_button.dart';
 import 'package:flutter/material.dart';
@@ -14,46 +15,34 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final counterCubit = BlocProvider.of<CounterCubit>(context);
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            BlocBuilder<CounterCubit, int>(
-              bloc: counterCubit,
-              builder: (context, counter) {
-                return Text(
-                  '$counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              },
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () => counterCubit.increment(),
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              BlocBuilder<CounterBloc, int>(
+                bloc: counterBloc,
+                builder: (context, counter) {
+                  return Text(
+                    '$counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                },
+              ),
+            ],
           ),
-          FloatingActionButton(
-            onPressed: () => counterCubit.decrement(),
-            tooltip: 'Increment',
-            child: const Icon(Icons.minimize),
-          ),
-        ],
-      ), 
-    );
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => IncAndDecButton()));
+        }));
   }
 }
